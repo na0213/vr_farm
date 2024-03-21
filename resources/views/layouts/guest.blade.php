81c81fbae0948434e7c4cf151283a92ad15b31fa
na0213 <natsumikan0213@gmail.com>
2024-03-21 18:55:16 +0900

    guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css','resources/css/swiper.min.css','resources/js/app.js'])
+        @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
             @if (Route::has('login'))
@@ -43,7 +43,7 @@
             </div>
         </div>
 
-        @vite(['resources/js/swiper-bundle.min.js'])
+        {{-- @vite(['resources/js/swiper-bundle.min.js']) --}}
         @vite(['resources/js/swiper.js'])
     </body>
 </html>
