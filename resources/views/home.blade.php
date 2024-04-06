8969def9ca936196f0a02129e8b890204851e45c
na0213 <natsumikan0213@gmail.com>
2024-04-07 05:01:59 +0900

    news

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,8 +61,7 @@
       </div>
     </div>
 
-    
-    <div class="w-full mt-10">
+    <div class="w-full mt-10 mb-10">
       <div class="flex flex-col md:flex-row">
         <div class="m-10 md:w-2/5">
           <img src="{{asset('storage/concept.jpg')}}" loading="lazy" alt="Photo by Martin Sanchez" class="h-full w-full object-cover object-center" />
@@ -83,37 +82,30 @@
             自分にも動物にも環境にも<br>
             ちょっといいコトを。<br>
             とってもいいモノを。<br>
-            皆さまに。<br>
           </p>
         </div>
       </div>
     </div>
-      {{-- <div class="mx-auto max-w-screen-xl px-4 md:px-8">
-        <div class="grid gap-8 md:grid-cols-2 lg:gap-12">
-          <div class="flex items-center">
-            <div class="py-8 h-64 overflow-hidden rounded-lg md:h-auto">
-              <img src="{{asset('storage/concept.jpg')}}" loading="lazy" alt="Photo by Martin Sanchez" class="h-full w-full object-cover object-center" />
-            </div>
+
+    <div class="story my-5">
+      <p>NEWS</p>
+    </div>
+      <div class="mx-auto max-w-screen-lg flex flex-wrap justify-center">
+        @foreach($news as $data)
+        <div class="max-w-xs m-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img class="w-full" src="{{$data['thumbnail']}}">
+          <div class="p-3">
+            {{$data['name']}}
+            <div class="mt-4">
+            <a href="{{$data['url']}}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
           </div>
-    
-          <div class="">
-            <h1 class="story">Concept</h1>
-    
-            <p class="story_text">
-              「アニマルウェルフェア」<br>「放牧」<br>「放牧」<br>
-              人と動物と環境にやさしい循環型牧場があります。<br />
-              そのような牧場を大切にしたい。<br />
-              日本の未来に残していきたい。<br />
-              普段毎日いただいているお肉・卵・牛乳など。<br />
-              少し意識を変えてみるきっかけ作りがしたい。<br /><br />
-              月に１度でもいい。<br />
-              循環型牧場の製品をいただくことで<br />
-              自分にも動物にも環境にも<br />
-              ちょっといいコトしませんか。<br />
-              とってもいいモノ食べませんか。<br />
-            </p>
           </div>
         </div>
-      </div> --}}
-    </div>
+        @endforeach
+      </div>
 </x-top-layout>
